49221dba58c03c3d12aa03000552d6b390e1f5ba
shoray singhal <shoryasinghall@gmail.com>
2022-05-26 20:36:33 +0530

    fine tuned the loaders

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,8 +1,7 @@
 const title = document.querySelector(".title");
 const video_btn = document.querySelector(".video-btn");
 const webcam_btn = document.querySelector(".webcam-btn");
-const body = document.querySelector("body");
-const loader = document.querySelector(".load-wrapper");
+
 video_btn.addEventListener("click", () => {
   window.location.href = "/video";
 });
@@ -15,7 +14,3 @@ title.addEventListener("click", function () {
   window.location.href = "/";
 });
 
-window.addEventListener("load", function () {
-  body.classList.remove("preload");
-  loader.style.display = "none";
-});
