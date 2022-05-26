c3c600b60a0dfdc3e2d34faaec871c71127f270d
shoray singhal <shoryasinghall@gmail.com>
2022-05-26 20:15:16 +0530

    loader added

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,7 +1,8 @@
 const title = document.querySelector(".title");
 const video_btn = document.querySelector(".video-btn");
 const webcam_btn = document.querySelector(".webcam-btn");
-
+const body = document.querySelector("body");
+const loader = document.querySelector(".load-wrapper");
 video_btn.addEventListener("click", () => {
   window.location.href = "/video";
 });
@@ -12,4 +13,9 @@ webcam_btn.addEventListener("click", () => {
 
 title.addEventListener("click", function () {
   window.location.href = "/";
+});
+
+window.addEventListener("load", function () {
+  body.classList.remove("preload");
+  loader.style.display = "none";
 });
