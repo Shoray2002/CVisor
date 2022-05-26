0c2189abfac10322c7c70f09db17cff222a4024c
shoray singhal <shoryasinghall@gmail.com>
2022-05-26 16:57:57 +0530

    dashboard half done

---
+++ dashboard/dashboard.js
@@ -0,0 +1,15 @@
+const title = document.querySelector(".title");
+const video_btn = document.querySelector(".video-btn");
+const webcam_btn = document.querySelector(".webcam-btn");
+
+video_btn.addEventListener("click", () => {
+  window.location.href = "/video";
+});
+
+webcam_btn.addEventListener("click", () => {
+  window.location.href = "/webcam";
+});
+
+title.addEventListener("click", function () {
+  window.location.href = "/";
+});
