91913dff46e60cd686c4729cb057f311dd2e5d73
shoray singhal <shoryasinghall@gmail.com>
2022-05-29 00:46:33 +0530

    multi detection enabled

--- stream/stream.js
+++ stream/stream.js
@@ -6,6 +6,8 @@ const stop = document.getElementById("stop");
 const selection = document.getElementById("select");
 const canvas = document.getElementById("canvas");
 let model_status = false;
+const SSD_MOBILENETV1 = "ssd_mobilenetv1";
+let selectedFaceDetector = SSD_MOBILENETV1;
 
 stop.addEventListener("click", () => {
   onPlay();
@@ -27,67 +29,21 @@ window.onload = function () {
 };
 
 async function loadModel() {
-  await faceapi.loadTinyFaceDetectorModel(
-    "https://www.rocksetta.com/tensorflowjs/saved-models/face-api-js/"
-  );
-  await faceapi.loadFaceLandmarkTinyModel(
-    "https://www.rocksetta.com/tensorflowjs/saved-models/face-api-js/"
-  );
-}
-
-function resizeCanvasAndResults(dimensions, canvas, results) {
-  console.log(dimensions);
-  dimensions instanceof HTMLVideoElement
-    ? faceapi.getMediaDimensions(dimensions)
-    : dimensions;
-  const width = faceapi.getMediaDimensions(dimensions)._width;
-  const height = faceapi.getMediaDimensions(dimensions)._height;
-  console.log(width, height);
-  canvas.width = width;
-  canvas.height = height;
-  return results.map((res) => res.forSize(width, height));
-}
-
-function drawDetections(dimensions, canvas, detections) {
-  const resizedDetections = resizeCanvasAndResults(
-    dimensions,
-    canvas,
-    detections
-  );
-  faceapi.drawDetection(canvas, resizedDetections);
-}
-function drawLandmarks(dimensions, canvas, results, withBoxes = false) {
-  const resizedResults = resizeCanvasAndResults(dimensions, canvas, results);
-  const drawBoxParams = {
-    lineWidth: 2,
-  };
-  if (withBoxes) {
-    console.log("with");
-    faceapi.drawDetection(
-      canvas,
-      resizedResults.map((det) => det.detection),
-      drawBoxParams
-    );
-  }
+  await faceapi.loadSsdMobilenetv1Model("../res/models");
 }
 
 async function onPlay() {
-  const options = new faceapi.TinyFaceDetectorOptions({
-    inputSize: 128,
-    scoreThreshold: 0.3,
-  });
-  let result = await faceapi
-    .detectSingleFace(video, options)
-    .withFaceLandmarks(true);
+  let minConfidence = 0.3;
+  const options = new faceapi.SsdMobilenetv1Options({ minConfidence });
+  let task = faceapi.detectAllFaces(video, options);
+  // const result = await faceapi.detectSingleFace(video, options);
+  const result = await task;
+  console.log(result);
   if (result) {
-    drawLandmarks(video, canvas, [result], true);
-    // document.getElementById("myDiv01").innerHTML =
-    //   "First of 68 face landmarks, x: " +
-    //   Math.round(result._unshiftedLandmarks._positions[0]._x) +
-    //   ", y: " +
-    //   Math.round(result._unshiftedLandmarks._positions[0]._y) +
-    //   "<br>";
+    const dims = faceapi.matchDimensions(canvas, video, true);
+    faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims));
   }
-
   setTimeout(() => onPlay());
 }
+
+// isFaceDetectionModelLoaded
