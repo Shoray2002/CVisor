69b0d2df37042668030c7ce44c50a30a23efeead
shoray singhal <shoryasinghall@gmail.com>
2022-05-29 03:43:03 +0530

    added loaders for models

--- stream/stream.js
+++ stream/stream.js
@@ -1,24 +1,28 @@
 const video = document.getElementById("webcam");
-const section = document.querySelector("section");
 const enableWebcamButton = document.getElementById("webcamButton");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const selection = document.getElementById("select");
 const canvas = document.getElementById("canvas");
+const body = document.querySelector("body");
+const loader = document.querySelector(".load-wrapper");
 let run_status = false;
 const SSD_MOBILENETV1 = "ssd_mobilenetv1";
 let selectedFaceDetector = SSD_MOBILENETV1;
 start.addEventListener("click", () => {
+  loadModel();
   run_status = true;
-  onPlay();
+  analyze();
+  body.classList.add("preload");
+  loader.style.display = "flex";
+  loader.style.backgroundColor = "#e4f0ff1b";
 });
+
 stop.addEventListener("click", () => {
   run_status = false;
 });
 
 window.onload = function () {
-  loadModel();
-  console.log("Model loaded");
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then(function (stream) {
@@ -28,25 +32,31 @@ window.onload = function () {
     .catch(function (err) {
       console.log("An error occurred: " + err);
     });
-  section.classList.remove("invisible");
+  body.classList.remove("preload");
+  loader.style.display = "none";
 };
 
 async function loadModel() {
   await faceapi.loadSsdMobilenetv1Model("../res/models");
 }
 
-async function onPlay() {
-  if (run_status) {
+async function analyze() {
+  if (run_status && faceapi.nets.ssdMobilenetv1.params) {
+    body.classList.remove("preload");
+    loader.style.display = "none";
+    console.log("Model loaded");
     let minConfidence = 0.3;
     const options = new faceapi.SsdMobilenetv1Options({ minConfidence });
     let task = faceapi.detectAllFaces(video, options);
-    // const result = await faceapi.detectSingleFace(video, options);
     const result = await task;
     if (result) {
       const dims = faceapi.matchDimensions(canvas, video, true);
       faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims));
     }
-    setTimeout(() => onPlay());
+    requestAnimationFrame(analyze);
+  } else if (!faceapi.nets.ssdMobilenetv1.params) {
+    console.log("Model not loaded");
+    requestAnimationFrame(analyze);
   } else {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   }
--- video/video.js
+++ video/video.js
@@ -3,12 +3,17 @@ const stop = document.querySelector("#stop");
 const video_input = document.querySelector("#video-input");
 const video = document.querySelector("#video");
 const canvas = document.querySelector("#canvas");
+const body = document.querySelector("body");
+const loader = document.querySelector(".load-wrapper");
 let run_status = false;
 const SSD_MOBILENETV1 = "ssd_mobilenetv1";
 let selectedFaceDetector = SSD_MOBILENETV1;
 start.addEventListener("click", () => {
   run_status = true;
   analyze();
+  body.classList.add("preload");
+  loader.style.display = "flex";
+  loader.style.backgroundColor = "#e4f0ff1b";
 });
 stop.addEventListener("click", () => {
   run_status = false;
@@ -33,6 +38,8 @@ window.onload = function () {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
   });
+  body.classList.remove("preload");
+  loader.style.display = "none";
 };
 
 async function loadModel() {
@@ -40,18 +47,21 @@ async function loadModel() {
 }
 
 async function analyze() {
-  if (run_status) {
+  if (run_status && faceapi.nets.ssdMobilenetv1.params) {
+    body.classList.remove("preload");
+    loader.style.display = "none";
+    console.log("Model loaded");
     let minConfidence = 0.3;
     const options = new faceapi.SsdMobilenetv1Options({ minConfidence });
     let task = faceapi.detectAllFaces(video, options);
-    // const result = await faceapi.detectSingleFace(video, options);
     const result = await task;
-    console.log(result);
-    const person_count = result.length;
     if (result) {
       const dims = faceapi.matchDimensions(canvas, video, true);
       faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims));
     }
+    requestAnimationFrame(analyze);
+  } else if (!faceapi.nets.ssdMobilenetv1.params) {
+    console.log("Model not loaded");
     requestAnimationFrame(analyze);
   } else {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
