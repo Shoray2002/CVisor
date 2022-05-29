e084d9071476744491ccb98f5ff3a8958873def1
shoray singhal <shoryasinghall@gmail.com>
2022-05-29 23:25:07 +0530

    added comments

--- stream/stream.js
+++ stream/stream.js
@@ -1,3 +1,4 @@
+// picking up elements from the DOM
 const video = document.getElementById("webcam");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
@@ -8,26 +9,34 @@ const body = document.querySelector("body");
 const loader = document.querySelector(".load-wrapper");
 let run_status = false;
 let selectedCam, mask_model;
+// face mask classifier model
 const model_URL = "https://teachablemachine.withgoogle.com/models/wJeEWVm8t/";
+
+// start analysis
 start.addEventListener("click", () => {
   loadModel();
   run_status = true;
   analyze();
+  // adding the loader
   body.classList.add("preload");
   loader.style.display = "flex";
   loader.style.backgroundColor = "#e4f0ff1b";
 });
 
+// stop analysis
 stop.addEventListener("click", () => {
   run_status = false;
 });
 
+// fires when a new camera is selected from the dropdown
 selection.addEventListener("change", function () {
   selectedCam = this.value;
   setUpCamera();
 });
 
+// fires when the page is loaded
 window.onload = function () {
+  // create a dropdown menu of all available cameras
   navigator.mediaDevices.enumerateDevices().then(function (devices) {
     devices.forEach(function (device) {
       if (device.kind === "videoinput") {
@@ -40,10 +49,12 @@ window.onload = function () {
     });
   });
   setUpCamera();
+  // removing the loader
   body.classList.remove("preload");
   loader.style.display = "none";
 };
 
+// function to set up the selected camera and start the video stream
 function setUpCamera() {
   navigator.mediaDevices
     .getUserMedia({
@@ -62,18 +73,23 @@ async function loadModel() {
 
 async function analyze() {
   if (run_status && faceapi.nets.ssdMobilenetv1.params && mask_model) {
+    // removing the loader when the model is loaded
     body.classList.remove("preload");
     loader.style.display = "none";
     let minConfidence = 0.3;
+    // create new model instance
     const options = new faceapi.SsdMobilenetv1Options({ minConfidence });
     let result, task;
     task = faceapi.detectAllFaces(video, options);
     result = await task;
     const dims = faceapi.matchDimensions(canvas, video, true);
+    // resize the canvas to match the video
     const resizedResults = faceapi.resizeResults(result, dims);
+    // extract the canvas of each detected face
     const facesCallback = faceapi.extractFaces(video, result);
     const faces = await facesCallback;
     if (result && faces.length > 0) {
+      // loop through each face and classify it
       faces.forEach((face, i) => {
         var faceStatus;
         const faceCordinates = resizedResults[i]["_box"];
@@ -83,7 +99,7 @@ async function analyze() {
           width: faceCordinates._width,
           height: faceCordinates._height,
         };
-
+        // draw a box around the face
         ctx.strokeStyle = "blue";
         ctx.lineWidth = 2;
         ctx.beginPath();
@@ -92,13 +108,16 @@ async function analyze() {
         ctx.textAlign = "start";
         ctx.textBaseline = "bottom";
         ctx.font = "bold 12px verdana, sans-serif";
+        // classify the face as either with_mask or without_mask
         mask_model.classify(face, (err, verdict) => {
           faceStatus = verdict[0].label.toString();
+          // if the face is not wearing a mask then draw a red label otherwise draw a green label
           ctx.fillStyle = faceStatus === "With_Mask" ? "green" : "red";
           ctx.fillText(faceStatus, box.x, box.y);
         });
       });
     }
+    // call analyze each frame
     requestAnimationFrame(analyze);
   } else if (!faceapi.nets.ssdMobilenetv1.params) {
     console.log("FACE DETECTION MODEL not loaded");
@@ -107,6 +126,7 @@ async function analyze() {
     console.log("MASK CLASSIFIER MODEL not loaded");
     requestAnimationFrame(analyze);
   } else {
+    // clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 }
--- video/video.js
+++ video/video.js
@@ -1,26 +1,38 @@
+// picking up elements from the DOM
 const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
 const video_input = document.querySelector("#video-input");
 const video = document.querySelector("#video");
 const canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
 const body = document.querySelector("body");
 const loader = document.querySelector(".load-wrapper");
+// face mask classifier model
 const model_URL = "https://teachablemachine.withgoogle.com/models/wJeEWVm8t/";
 let run_status = false;
 let mask_model;
+
+// start analysis
 start.addEventListener("click", () => {
-  loadModel();
-  run_status = true;
-  analyze();
-  body.classList.add("preload");
-  loader.style.display = "flex";
-  loader.style.backgroundColor = "#e4f0ff1b";
+  if (video.src) {
+    loadModel();
+    run_status = true;
+    analyze();
+    // adding the loader
+    body.classList.add("preload");
+    loader.style.display = "flex";
+    loader.style.backgroundColor = "#e4f0ff1b";
+  }
 });
+
+// stop analysis
 stop.addEventListener("click", () => {
   run_status = false;
 });
 
+// fires when the page is loaded
 window.onload = function () {
+  // when the video is uploaded
   video_input.addEventListener("change", function () {
     let file = this.files[0];
     let blobURL = URL.createObjectURL(file);
@@ -33,9 +45,11 @@ window.onload = function () {
     }
 
     video.playbackRate = 0.25;
+    // resizing the canvas to match the video
     canvas.width = video.width;
     canvas.height = video.height;
   });
+  // removing the loader
   body.classList.remove("preload");
   loader.style.display = "none";
 };
@@ -47,18 +61,23 @@ async function loadModel() {
 
 async function analyze() {
   if (run_status && faceapi.nets.ssdMobilenetv1.params && mask_model) {
+    // removing the loader when the model is loaded
     body.classList.remove("preload");
     loader.style.display = "none";
     let minConfidence = 0.3;
+    // create new model instance
     const options = new faceapi.SsdMobilenetv1Options({ minConfidence });
     let result, task;
     task = faceapi.detectAllFaces(video, options);
     result = await task;
     const dims = faceapi.matchDimensions(canvas, video, true);
+    // resize the canvas to match the video
     const resizedResults = faceapi.resizeResults(result, dims);
+    // extract the canvas of each detected face
     const facesCallback = faceapi.extractFaces(video, result);
     const faces = await facesCallback;
     if (result && faces.length > 0) {
+      // loop through each face and classify it
       faces.forEach((face, i) => {
         var faceStatus;
         const faceCordinates = resizedResults[i]["_box"];
@@ -68,7 +87,7 @@ async function analyze() {
           width: faceCordinates._width,
           height: faceCordinates._height,
         };
-        let ctx = canvas.getContext("2d");
+        // draw a box around the face
         ctx.strokeStyle = "blue";
         ctx.lineWidth = 2;
         ctx.beginPath();
@@ -77,13 +96,16 @@ async function analyze() {
         ctx.textAlign = "start";
         ctx.textBaseline = "bottom";
         ctx.font = "bold 12px verdana, sans-serif";
+        // classify the face as either with_mask or without_mask
         mask_model.classify(face, (err, verdict) => {
           faceStatus = verdict[0].label.toString();
+          // if the face is not wearing a mask then draw a red label otherwise draw a green label
           ctx.fillStyle = faceStatus === "With_Mask" ? "green" : "red";
           ctx.fillText(faceStatus, box.x, box.y);
         });
       });
     }
+    // call analyze each frame
     requestAnimationFrame(analyze);
   } else if (!faceapi.nets.ssdMobilenetv1.params) {
     console.log("FACE DETECTION MODEL not loaded");
@@ -92,6 +114,7 @@ async function analyze() {
     console.log("MASK CLASSIFIER MODEL not loaded");
     requestAnimationFrame(analyze);
   } else {
+    // clear the canvas
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   }
 }
